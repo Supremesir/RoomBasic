91ed19c57168d1b16ca9a616be22b140778a1f1b
Haofan Fang <haofanfang@gmail.com>
2020-03-30 20:54:17 +0800

    update Recyclerview with CardView

--- app/src/main/java/com/supremesir/roombasic/MyAdapter.java
+++ app/src/main/java/com/supremesir/roombasic/MyAdapter.java
@@ -28,14 +28,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View itemView = layoutInflater.inflate(R.layout.cell_normal, parent, false);
+//        View itemView = layoutInflater.inflate(R.layout.cell_normal, parent, false);
+        View itemView = layoutInflater.inflate(R.layout.cell_card, parent, false);
         return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Word word = allWords.get(position);
-        holder.textViewNumber.setText(String.valueOf(word.getId()));
+//        holder.textViewNumber.setText(String.valueOf(word.getId()));
+        holder.textViewNumber.setText(String.valueOf(position + 1));
         holder.textViewEnglish.setText(word.getWord());
         holder.textViewChinese.setText(word.getChineseMeaning());
     }
